3109acc236037338f18487a12b418d2e7c7d3717
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-02-19 11:30:54 -0300

    Add files via upload

---
+++ CalculadoraTeste01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTeste01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valor1;
+            double valor2;
+            double total;
+
+
+
+            Console.Write("Digite o valor 1: ");
+
+            valor1 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.Write("Digite o valor 2:");
+
+            valor2 = Convert.ToDouble( Console.ReadLine() );
+            total = valor1 + valor2;
+
+
+            Console.Write("Resultado dasoma é: " + total);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
